2514979aeb96c6eea26c9e6a0e6e104ee5f49d69
yassin <58929848+yasin6606@users.noreply.github.com>
2020-08-07 16:26:19 +0430

    remove ButtonComponent in Header Component in frontend

--- frontend/src/Main/Components/Header/Header.js
+++ frontend/src/Main/Components/Header/Header.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 
 class Header extends Component {
     render() {
         return (
             <>
-                <Container>
+                <Container className="col-12">
                     <Row>
                         <Col>
-                            
+                            <Navbar className="border-bottom py-4 d-flex justify-content-around">
+                                <Navbar.Collapse>
+                                    <Nav>
+
+                                    </Nav>
+                                    <Nav>
+
+                                    </Nav>
+                                </Navbar.Collapse>
+                                <Navbar.Collapse className="d-flex justify-content-end">
+                                    <Nav>
+
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Navbar>
                         </Col>
                     </Row>
                 </Container>
