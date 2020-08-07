de394f1bf82eac4f5cbcfb49dd9d7bbfc0207e26
yassin <58929848+yasin6606@users.noreply.github.com>
2020-08-07 19:03:40 +0430

    create Skills component

---
+++ frontend/src/Main/Components/SkillsPart/Skills.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+class Skills extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    };
+};
+
+export default Skills;
