dd933926b0986347ff7a10d70987cec5a0600b05
yassin <58929848+yasin6606@users.noreply.github.com>
2020-08-07 13:59:54 +0430

    config App.js component and add init Header component to App component in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './Main/Components/Header/Header';
 
 function App() {
   return (
     <>
+      <Container>
+        <Row>
+          <Col>
+            <Row>
+              <Col>
+                <Header />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
